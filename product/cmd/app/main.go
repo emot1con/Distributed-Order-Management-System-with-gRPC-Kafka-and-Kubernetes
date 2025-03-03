4667f9feb26b113d3f33e94c4b4f22566f7b4ca6
emot1con <arqanpurusa@gmail.com>
2025-03-03 09:46:55 +0700

    initializing product service

---
+++ product/cmd/app/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"product_service/cmd/db"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	if _, err := db.Connect(); err != nil {
+		logrus.Fatalf("Failed to connect to database: %v", err)
+	}
+	srv := http.Server{
+		Addr:    ":40001",
+		Handler: nil,
+	}
+
+	logrus.Info("Starting server on port 40001")
+
+	if err := srv.ListenAndServe(); err != nil {
+		logrus.Fatalf("Failed to start server: %v", err)
+	}
+}
---
+++ product/cmd/db/database.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() (*gorm.DB, error) {
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPass := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dsn := fmt.Sprintf("host=postgres user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Jakarta", dbUser, dbPass, dbName)
+
+	for i := 0; i < 5; i++ {
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			db.AutoMigrate()
+			return db, nil
+		}
+
+		logrus.Warnf("Database connection failed. Retrying in 5 seconds... (%d/5)", i+1)
+		time.Sleep(5 * time.Second)
+	}
+	return nil, fmt.Errorf("failed to connect to database")
+}
